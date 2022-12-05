46c3883f06cb5bb1774fd9d7fe5e1c8307e3ec24
LeoHedlund1 <leohed02@edu.umea.se>
2022-12-05 12:24:32 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    let todoItems = [];
+    let newItem;
+
+    function AddItem(){
+        if (newItem != undefined){
+            todoItems.push(newItem);
+            console.log(todoItems);
+        }
+        todoItems = todoItems;
+    }
+
+    function RemoveItem(name){
+        todoItems.splice(todoItems.indexOf(name), 1);
+        todoItems = todoItems;
+    }
+</script>
+<main>
+    <h1>TODO</h1>
+    <body>
+        <div class="input">
+            <button on:click={AddItem}>Add</button>
+            <input type="text" id="newitem" bind:value={newItem}>
+        </div>
+        <div class="list">
+            <ul>
+                {#each todoItems as item}
+                    <li>
+                        {item}
+                        <button on:click={RemoveItem(item)}>Remove</button>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </body>
+</main>
+<style>
+    h1 {
+        font-size: 50px;
+        text-align: center;
+    }
+
+    ul {
+        font-size: 30px;
+    }
+
+    .input {
+        text-align: center;
+    }
+</style>
